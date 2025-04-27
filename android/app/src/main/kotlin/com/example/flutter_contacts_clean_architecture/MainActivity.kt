4982799020f406862a8ee307b4f45acd3a014f1e
JJeong Kim <64342840+jeongwoo0427@users.noreply.github.com>
2025-04-27 18:23:01 +0900

    chore: project recreate

--- android/app/src/main/kotlin/com/example/flutter_contacts_clean_architecture/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_contacts_clean_architecture/MainActivity.kt
@@ -2,4 +2,4 @@ package com.example.flutter_contacts_clean_architecture
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity()
